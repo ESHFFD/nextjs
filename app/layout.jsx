b7d415301ab04fdcd26a30eebf03f481e5bbb058
ESHFFD <ehsanshamsfallah@gmail.com>
2023-10-02 19:29:49 +0330

    create structure of project

---
+++ app/layout.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "@styles/globals.css";
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
